0feebf46b4d596a983e9f0d2f8012b13b6f5a955
Giwon <leecode99@gmail.com>
2023-02-05 23:45:35 -0500

    minor fixes

--- src/components/Email.js
+++ src/components/Email.js
@@ -1,6 +1,9 @@
 
 import React, { useRef } from "react";
 import emailjs from '@emailjs/browser';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Email() {
     const form = useRef();
@@ -18,14 +21,28 @@ export default function Email() {
     };
 
     return (
-        <form ref={form} onSubmit={sendEmail}>
-            <label>Name</label>
-            <input type="text" name="name" /><br/>
-            <label>Email</label>
-            <input type="email" name="user_email" /><br/>
-            <label>Message</label>
-            <textarea name="message" /><br/>
-            <input type="submit" value="Send" />
-        </form>
+
+        <Form ref={form} onSubmit={sendEmail} className="EmailForm">
+            {/* <h5 style="font-style:italic;">Inquries</h5><hr></hr> */}
+            <Form.Group className="mb-3" controlId="formBasicName">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="text" name="name" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" name="user_email" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Label>Message</Form.Label>
+                <Form.Control as="textarea" name="message" rows={3} />
+            </Form.Group>
+            
+            <Button variant="secondary" type="submit">
+                Submit
+            </Button>
+
+        </Form>
+
     );
 }
